4de5cd6a41bcd0e2c39d389afba3cd83289ec7c4
naix125 <naix125@gmail.com>
2023-07-31 07:02:11 +0400

    Start header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import s from './Header.module.css';
+// import s from './Header.module.css';
+import './Header.css'; 
 const Header =()=>{
-    return (
-    <div className={s.header}> 
-  sss
-    </div>
+    return (<header className="header">
+      <div className="logo">
+        <img src="icon.png" alt="logo" className="logo-image" />
+      </div>
+      
+      <div className="search">
+          <input type="text" className="search-input" />
+        </div>
+        <div className="icons">
+          <img src="bell.png" alt="bell" className="notification-icon" />
+          <img src="music.png" alt="music" className="music-icon" />
+        </div>
+        <div className="profiles">
+          <img src="app-icon.png" alt="app icon" className="app-icon" />
+          <img src="avatar.png" alt="avatar" className="avatar" />
+        </div>
+  
+        </header>
     )
 }
+
+
 
 
 
